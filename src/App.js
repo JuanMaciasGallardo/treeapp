b905b24a9f616b9296414a466a37a96d2fdb3a7d
juanmaciasgallardo <juan.macias@metadata.es>
2024-04-23 14:50:22 +0200

    Inicializa el repositorio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { MeaButo } from './MeaButo';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='Titulus'>Bene ventus ad meam applicationem</h1>
+      <MeaButo />
+    </div>
+  );
+}
+
+export default App;
